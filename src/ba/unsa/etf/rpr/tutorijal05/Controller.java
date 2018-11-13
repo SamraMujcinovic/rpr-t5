ecdb381a82cd083335145a6ac44a3cea883ed38d
SamraMujcinovic <ssmujcinovi1@etf.unsa.ba>
2018-11-13 09:38:59 +0100

    Graficki interfejs uradjen

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,28 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
 
+    public Button equalsBtn;
+    public TextField inputField;
+    public int vr, vr2;
+
+    public void ispisi(ActionEvent actionEvent) {
+        System.out.println("Lolly " + vr);
+    }
+
+    /*
+    add()
+        operator = '+';
+
+    equals()
+        vr = vr operaor vr2
+    */
+
+    public void set1(ActionEvent actionEvent) {
+        vr = 1;
+    }
 }
